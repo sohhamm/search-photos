6f47e95c761268113f7e0e3334c06791797677a8
Soham Sarkar <soham.ss1@gmail.com>
2021-08-09 09:30:06 +0530

    minor change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,31 @@ import * as React from 'react';
 import { MdSearch } from 'react-icons/md';
 import {
   Flex,
+  Heading,
   Icon,
   Input,
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react';
+import ComboBox from './ComboBox';
 
 export default function Header() {
   return (
-    <Flex w="100%" justify="center" align="center">
-      <InputGroup w="30%">
+    <Flex
+      w="100%"
+      justify="center"
+      align="center"
+      direction="column"
+      bg="bisque"
+    >
+      {/* <InputGroup w="30%">
         <Input />
         <InputRightElement>
           <Icon as={MdSearch} />
         </InputRightElement>
-      </InputGroup>
+      </InputGroup> */}
+      <Heading>Search Photos</Heading>
+      <ComboBox />
     </Flex>
   );
 }
